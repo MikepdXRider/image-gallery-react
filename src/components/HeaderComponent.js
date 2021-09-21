077f1b6e091cb74d4217a0452b5010e08eac3400
Michael Rider <mikepdxrider@gmail.com>
2021-09-21 13:38:06 -0700

    adds header component

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class HeaderComponent extends Component {
+    render() {
+        return (
+            <header className='flex-container'>
+                <h1>Puppy Gallery</h1>
+            </header>
+        )
+    }
+}
